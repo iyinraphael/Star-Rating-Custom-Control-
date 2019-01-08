05343b3b1e9c46a24552e2b5b7659f2d394cd2dd
iyinraphael <iyinraphael@gmail.com>
2019-01-08 11:10:48 -0800

    created labels and contraints for labels to insert strings stars into

--- Star Rating (Custom Control)/CustomControl.swift
+++ Star Rating (Custom Control)/CustomControl.swift
@@ -12,5 +12,46 @@ class CustomControl: UIControl {
     
     var value: Int = 1
     
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount: Int = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
     
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        
+        setUp()
+    }
+    
+    
+    func setUp(){
+        
+        var labels: [UILabel] = []
+        
+        for num in 1...5{
+            
+            let label: UILabel = UILabel()
+            self.addSubview(label)
+            
+            label.frame.size = CGSize(width: componentDimension, height: componentDimension)
+            let x = CGFloat(integerLiteral: (8 * num))
+            label.layer.position = CGPoint(x: x, y: 0)
+            
+            label.font = UIFont.systemFont(ofSize: 32.0)
+            label.text = "✯"
+            
+            label.textColor = componentInactiveColor
+            
+            label.tag = num
+            labels.append(label)
+            NSLog("\(label)")
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
 }
